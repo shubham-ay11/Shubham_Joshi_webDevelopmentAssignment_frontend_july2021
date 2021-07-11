17c21582480d4f15337c2f6b0d54029e52c1b9c1
Shubham Joshi <shubhjoshi114@gmail.com>
2021-07-12 02:18:56 +0530

    Login page is completed

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const express=require('express');
 const app=express();
 
 app.use(express.static("public"));
-app.get("/",function(req,res){
+app.get("/",(req,res)=>{
     
     res.sendFile(__dirname+"/index.html")
 });
+
+app.get("/login",(req,res)=>{
+    
+    res.sendFile(__dirname+"/login.html");
+})
 
 app.listen(3000,console.log("Server stated at Port 3000"));
 
